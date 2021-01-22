3ace738354173395a2f5a9639e9ade64d2b40c1b
MDCHAMP <max.champneys@gmail.com>
2021-01-22 16:26:22 +0000

    cubic stiffness

---
+++ src/toybox/nonlinearities.py
@@ -0,0 +1,14 @@
+'''
+Common nonlinearities included here
+'''
+
+import numpy as np
+
+def cubic_stifness(dofs, kn3=None):
+    if kn3 is None:
+        kn3 = np.zeros((dofs, dofs))
+        kn3[0,0,] = 1500    
+    def N(self, t, y, ydot):
+        return np.dot(kn3, y**3)
+    return N
+
