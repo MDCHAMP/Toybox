d693215ca6165a2cffa62e2e1d960c193eda7de8
MDCHAMP <max.champneys@gmail.com>
2021-01-22 17:07:25 +0000

    better test coverage

--- tests/toybox/test_toys.py
+++ tests/toybox/test_toys.py
@@ -17,14 +17,34 @@ def test_requrements_satisfied():
     assert 'numpy' in sys.modules
 
 
+
 @pytest.mark.parametrize('s', systems)
-def test_load_and_simulate(s):
+def test_load(s):
     assert s.M.shape[0] == s.M.shape[1]
     assert s.C.shape[0] == s.C.shape[1]
     assert s.K.shape[0] == s.K.shape[1]
-    ts = np.linspace(0, 10, 100)
+
+@pytest.mark.parametrize('s', systems)
+def test_simulate(s):
+    ts = np.linspace(0, 10, 1000)
     w0 = np.zeros((2*s.dofs,))
     w0[0] = 1
-    output = s.simulate(w0, ts,).T
-    for sig in output:
+    s.simulate(w0, ts)
+    for sig in s.response:
         assert sig.shape == ts.shape
+        assert not any(np.isnan(sig))
+
+@pytest.mark.parametrize('s', systems)
+def test_normalise(s):
+    ts = np.linspace(0, 10, 1000)
+    w0 = np.zeros((2*s.dofs,))
+    w0[0] = 1
+    s.simulate(w0, ts)
+    s.normalise()
+    for sig in s.response:
+        assert (np.std(sig) - 1) < 10**-3
+        assert (np.mean(sig) - 0) < 10**-3 
+    s.denormalise()
+    for sig, scale, offset in zip(s.response, s.scale, s.offset):
+        assert (np.std(sig) - scale) < 10**-3
+        assert (np.mean(sig) - offset) < 10**-3
